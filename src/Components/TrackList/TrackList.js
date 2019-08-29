679a150f5af01843469f4e8603f00bba4ccd18c2
blysca <blysca@gmail.com>
2019-08-29 05:35:57 +0300

    Passed Down Search Result and Render Result List

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TrackList.css';
+import {Track} from '../Track/Track';
+
+export class TrackList extends React.Component {
+  render() {
+
+    console.log(this.props.tracks)
+    return (
+      <div className="TrackList">
+        {this.props.tracks.map( trackItem =>
+            <Track key={trackItem.id} track={trackItem}/>
+        )}
+
+      </div>
+    );
+  }
+}
